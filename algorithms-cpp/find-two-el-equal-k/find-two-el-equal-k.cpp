57c4da2aa05554725c0da2494700737054f9abc8
denis.lyubo <denis.lyubo@gmail.com>
2023-04-12 15:19:27 +0300

    fix

--- algorithms-cpp/find-two-el-equal-k/find-two-el-equal-k.cpp
+++ algorithms-cpp/find-two-el-equal-k/find-two-el-equal-k.cpp
@@ -11,7 +11,14 @@
 
 using namespace std;
 
+template<typename T>
+concept SupportFirstSecond = requires(T val) {
+	val.first;
+	val.second;
+};
+
 template <typename T>
+requires SupportFirstSecond<T>
 std::ostream& operator<< (std::ostream& out, std::pair<T, T> const& data) {
 	out << data.first << ":" << data.second << ";";
 
@@ -19,6 +26,7 @@ std::ostream& operator<< (std::ostream& out, std::pair<T, T> const& data) {
 }
 
 template<typename T>
+requires SupportFirstSecond<T>
 struct pair_hash {
 	size_t operator() (const T& p) const noexcept {
 		return ((size_t)p.first << 32) | p.second;
@@ -26,9 +34,10 @@ struct pair_hash {
 };
 
 template <typename T>
+requires SupportFirstSecond<T>
 struct Comparator {
 	bool operator() (const T& a, const T& b) const noexcept {
-		if (a == b || (a.first == b.second && a.second == b.first))
+		if ((a.first == b.first && a.second == b.second) || (a.first == b.second && a.second == b.first))
 			return true;
 
 		return false;
@@ -44,9 +53,9 @@ auto twoSum(const vector<int>& arr, int k) {
 	auto len = arr.size();
 
 	for (auto i = 0; i < len; ++i) 
-		for (auto j = 0; j < len; ++j) {
+		for (auto j = i+1; j < len; ++j) {
 			if ((arr[i] + arr[j]) == k) {
-				res.insert(make_pair(arr[i], arr[j]));
+				res.insert(make_pair(min(arr[i], arr[j]), max(arr[i], arr[j])));
 			}
 		}
 
@@ -56,9 +65,9 @@ auto twoSum(const vector<int>& arr, int k) {
 
 int main()
 {
-	vector<int> input{ -1, 2,6, 7, 12, 14 };
+	vector<int> input{ -1, 2,6, 7, 8, 12, 14, 15, 16 };
 
-	auto res = twoSum(input, 6);
+	auto res = twoSum(input, 15);
 
 	cout << "Found results: " << endl;
 	for (const auto& el : res) {
