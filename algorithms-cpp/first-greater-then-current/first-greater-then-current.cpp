fed49a5d9e1957433a4aaa35e34f3ee709275aaf
denis.lyubo <denis.lyubo@gmail.com>
2023-04-12 18:15:53 +0300

    added o(n) solution

--- algorithms-cpp/first-greater-then-current/first-greater-then-current.cpp
+++ algorithms-cpp/first-greater-then-current/first-greater-then-current.cpp
@@ -4,6 +4,7 @@
 #include "first-greater-then-current.h"
 
 #include <vector>
+#include <stack>
 
 
 using namespace std;
@@ -30,6 +31,43 @@ vector<int> fst_grtr_cur(const vector<int>&a)
 	return res;
 }
 
+vector<int> fst_grtr_cur_1(const vector<int>& a)
+{
+	const auto len = a.size();
+	vector<int> res(len, 0);
+	stack<pair<int, int>> index_temp;
+
+
+	for (auto i = static_cast<int>(len)-1; i >=0; --i)
+	{
+		if (i == len - 1) 
+		{
+			index_temp.emplace(make_pair(i, a[i]));
+			continue;
+		}
+
+
+		while (index_temp.size())
+		{
+			if (a[i] >= index_temp.top().second)
+				index_temp.pop();
+			else
+				break;
+		}
+
+		if (index_temp.size())
+		{
+			res[i] = index_temp.top().first - i;
+		}
+		else
+			res[i] = 0;
+
+		index_temp.emplace(make_pair(i, a[i]));		
+	}
+
+	return res;
+}
+
 int main()
 {
 	vector<int> input{ -1, -7, 0, -2, 2, 4, 3, 3, 6 };
@@ -40,13 +78,23 @@ int main()
 	}
 	cout << endl;
 
+	cout << "Naive O(n^2) approach: " << endl;
 	auto res = fst_grtr_cur(input);
 
 	for (const auto& i : res)
 	{
 		cout << i << " ";
 	}
-	cout << endl;	
+	cout << endl;
+
+	cout << "Stack based O(n) approach: " << endl;
+	auto res1 = fst_grtr_cur_1(input);
+
+	for (const auto& i : res1)
+	{
+		cout << i << " ";
+	}
+	cout << endl;
 
 	return 0;
 }
